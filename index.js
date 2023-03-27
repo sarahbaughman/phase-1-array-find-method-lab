928bb1e971b9a1a1a9212cb53198ec42bfec3b85
Sarah Baughman <sarahbaughman92@gmail.com>
2023-03-27 16:31:51 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+const records = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+]
+
+function superbowlWin(record) {
+    let oneWay = record.find(element => element.result === "W")
+    if (oneWay)
+        return oneWay.year
+    else return oneWay
+}
+
+superbowlWin(records)
+
+
